769417b076949e583ce1331edd5a8fe7c3fefd68
Ishant09Sadhawani <ishantsadhawani765@gmail.com>
2025-09-13 21:24:20 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ishant Sadhawani",
+    author_email="ishantsadhawani765@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
